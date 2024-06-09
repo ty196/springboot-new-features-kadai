f9cb3dfe187f216ef9c511b4abc1c277344e1a5b
ty196 <162804784+ty196@users.noreply.github.com>
2024-06-09 10:31:01 +0900

    springboot kadai

---
+++ kadai_002/samaraitravel/src/main/java/com/example/samuraitravel/SamaraitravelApplication.java
@@ -0,0 +1,13 @@
+package com.example.samuraitravel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamaraitravelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamaraitravelApplication.class, args);
+	}
+
+}
